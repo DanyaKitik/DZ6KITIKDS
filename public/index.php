f5d8a021198a21629ebb647078297413327c4aee
DanyaKitik <kitik955@gmail.com>
2020-07-23 17:40:40 +0300

    commit 3

--- public/index.php
+++ public/index.php
@@ -3,11 +3,11 @@
 require_once '../vendor/autoload.php';
 
 $autoloader = new Autoloader();
-//$autoloader->addNamespace("app","../src");
-//$autoloader->addNamespace("app2","../src");
+$autoloader->addNamespace("app","../src");
+$autoloader->addNamespace("app2","../src");
 $autoloader->addNamespace("app3","../src");
 $autoloader->register();
 
-//$color1 = new app\Color;
-//$color2 = new app2\Color2;
+$color1 = new app\Color;
+$color2 = new app2\Color2;
 $color3 = new app3\asd\Color3;
--- src/asd/Color3.php
+++ src/asd/Color3.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app3;
+namespace app3\asd;
 
 class Color3{
     public function __construct()
